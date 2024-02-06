753d233be3c0077ce1a6fd2eb81914bc0974c032
NiahKanto <fanjavaniah@gmail.com>
2024-02-06 23:54:32 +0100

    Authentification dans bdd

--- routes/login.js
+++ routes/login.js
@@ -2,23 +2,23 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 var router = express.Router();
+const {connectToDB} = require('../db/db');
 
-const users = [
-    { id: 1, username: 'user1', password: '$2a$10$nLsUFfQ0vzLbQgPLutm5ROaTSkxj.lZJtfb3ii3IEYP/aJIKNFw1m' } // hashed password: "password"
-];
-
+const collectionName = 'user';
 // Login route
 router.post('/', async (req, res) => {
     const { username, password } = req.body;
-    const user = users.find(u => u.username === username);
+    const db = await connectToDB();
+    const collection = db.collection(collectionName);
+    const user = await collection.findOne({nom: username})
 
     if (!user) {
         return res.status(401).json({ message: 'Utilisateur non trouvé' });
     }
 
     try {
-        if (await bcrypt.compare(password, user.password)) {
-            const token = jwt.sign({ username: user.username, id: user.id }, 'secret', { expiresIn: '1h' });
+        if (await bcrypt.compare(password, user.mdp)) {
+            const token = jwt.sign({ username: user.nom, id: user._id }, 'secret', { expiresIn: '1h' });
             res.json({ token });
         } else {
             res.status(401).json({ message: 'Mot de passe incorrect' });
